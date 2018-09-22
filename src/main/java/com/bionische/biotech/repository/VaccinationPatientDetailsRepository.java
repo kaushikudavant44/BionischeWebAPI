712dc9d493255e4173183f910dc8b5c13e062be5
Jarves <Jarves@DESKTOP-TLMVIJ2>
2018-09-22 11:59:44 +0530

    //kaushik

--- src/main/java/com/bionische/biotech/repository/VaccinationPatientDetailsRepository.java
+++ src/main/java/com/bionische/biotech/repository/VaccinationPatientDetailsRepository.java
@@ -8,7 +8,7 @@ import com.bionische.biotech.model.VaccinationPatientDetails;
 
 public interface VaccinationPatientDetailsRepository extends JpaRepository<VaccinationPatientDetails, Integer>{
 
-	@Query(value="SELECT p.patient_id,p.f_name,p.m_name,p.l_name,p.gender,p.address,p.blood_group, p.birth_date,p.del_status FROM patient_details p WHERE p.patient_id=1 AND p.del_status=0",nativeQuery=true)
+	@Query(value="SELECT p.patient_id,p.f_name,p.m_name,p.l_name,p.gender,p.address,p.blood_group, p.birth_date,p.del_status FROM patient_details p WHERE p.patient_id=:patientId AND p.del_status=0",nativeQuery=true)
 	VaccinationPatientDetails getPatientByPatientId(@Param("patientId")int patientId);
 	
 }
