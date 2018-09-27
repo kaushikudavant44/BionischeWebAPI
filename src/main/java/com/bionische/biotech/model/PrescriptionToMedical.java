db0bad4df6cdc5d89c235e04b6f830d5084e6913
Admin <Admin@Bionische2>
2018-09-27 18:32:00 +0530

    //by amruta

--- src/main/java/com/bionische/biotech/model/PrescriptionToMedical.java
+++ src/main/java/com/bionische/biotech/model/PrescriptionToMedical.java
@@ -20,7 +20,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
 @Entity
-@Table(name="doctor_appointment")
+@Table(name="prescription_to_medical")
 @EntityListeners(AuditingEntityListener.class)
 
 public class PrescriptionToMedical {
