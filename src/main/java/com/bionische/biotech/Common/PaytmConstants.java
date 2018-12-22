c0c33c2072710951b83a0bc649a22720dd936e46
Dell <Dell@Dell-PC>
2018-12-22 16:11:51 +0530

    Paytm //atul

--- src/main/java/com/bionische/biotech/Common/PaytmConstants.java
+++ src/main/java/com/bionische/biotech/Common/PaytmConstants.java
@@ -2,12 +2,19 @@ package com.bionische.biotech.Common;
 
 public class PaytmConstants {
 
+//	public final static String MERCHANT_KEY = "8L#sS_SKGEgsMR%U"; // %_GHVWhEQWMel&Bq
 	public final static String MERCHANT_KEY = "%_GHVWhEQWMel&Bq";
+//	public final static String MID = "NpelrP68647637633893"; //UtTcWW96827408585088 
 	public final static String MID = "UtTcWW96827408585088";
 
 	public final static String INDUSTRY_TYPE_ID = "Retail";
-	public final static String CHANNEL_ID = "WEB";
+	public final static String CHANNEL_ID = "WAP";
 	public final static String WEBSITE = "DEFAULT";
 	public final static String PAYTM_URL = "https://securegw.paytm.in/theia/processTransaction";
+	public final static String CALLBACK_URL = "https://securegw.paytm.in/theia/paytmCallback?ORDER_ID=";
 
+	
 }
+
+
+
