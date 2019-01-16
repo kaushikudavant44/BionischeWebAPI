5d6a92de8d871334d3378dd02d9f905c732eebba
Jarves <Jarves@DESKTOP-TLMVIJ2>
2019-01-16 18:58:37 +0530

    //kaushik

--- src/main/java/com/bionische/biotech/service/UpdateTokenAndDeviceTypeService.java
+++ src/main/java/com/bionische/biotech/service/UpdateTokenAndDeviceTypeService.java
@@ -3,5 +3,7 @@ package com.bionische.biotech.service;
 public interface UpdateTokenAndDeviceTypeService {
 	
 	public int updateDoctorTokenAslocationByDoctorId(int doctorId,String token,int deviceType);
+	
+	public int updatePatientTokenAsString2ByPatientId(int patientId,String token,int deviceType);
 
 }
--- src/main/java/com/bionische/biotech/service/UpdateTokenAndDeviceTypeServiceImpl.java
+++ src/main/java/com/bionische/biotech/service/UpdateTokenAndDeviceTypeServiceImpl.java
@@ -4,18 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bionische.biotech.repository.DoctorDetailsRepository;
+import com.bionische.biotech.repository.PatientDetailsRepository;
 
 @Service
-public class UpdateTokenAndDeviceTypeServiceImpl implements UpdateTokenAndDeviceTypeService{
+public class UpdateTokenAndDeviceTypeServiceImpl implements UpdateTokenAndDeviceTypeService {
 
 	@Autowired
 	DoctorDetailsRepository doctorDetailsRepository;
+	
+	@Autowired
+	PatientDetailsRepository patientDetailsRepository;
 	
 	@Override
 	public int updateDoctorTokenAslocationByDoctorId(int doctorId, String token, int deviceType) {
 		
 		int updateTokenAndDevice=doctorDetailsRepository.updateDoctorTokenAslocationByDoctorId(doctorId,token,deviceType);
 		
+		
+		return updateTokenAndDevice;
+	}
+
+	@Override
+	public int updatePatientTokenAsString2ByPatientId(int patientId, String token, int deviceType) {
+		
+		int updateTokenAndDevice=patientDetailsRepository.updatePatientTokenAsString2ByPatientId(patientId, token, deviceType);
 		
 		return updateTokenAndDevice;
 	}
