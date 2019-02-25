b0c383805c5656282069024a0e6c311651cfad5c
Admin <Admin@DESKTOP-9LBB3GJ>
2019-02-25 16:54:54 +0530

    //Ganesh :- Lab issues

--- src/main/java/com/bionische/biotech/repository/GetLabBillDetailsRepository.java
+++ src/main/java/com/bionische/biotech/repository/GetLabBillDetailsRepository.java
@@ -10,7 +10,7 @@ public interface GetLabBillDetailsRepository extends JpaRepository<GetLabBillDet
 
 	@Query(value="SELECT a.lab_app_id, a.patient_id, a.lab_id, a.amount_type, a.payment_status, a.amount, a.discount, a.paid_amount, " + 
 			" a.txn_id, a.order_id, a.payment_date, CONCAT(p.f_name,' ',p.l_name)AS patient_name, l.lab_name, a.test_id_list,GROUP_CONCAT( t.lab_test_name SEPARATOR ', ')AS lab_test_name, p.email,l.contact, " + 
-			"l.address FROM t_lab_appointment_details a, lab_tests t, lab_details l, patient_details p WHERE a.payment_status!=0 AND a.lab_app_id=:id  " + 
+			"l.address FROM t_lab_appointment_details a, lab_tests t, lab_details l, patient_details p WHERE FIND_IN_SET(t.lab_test_id, a.test_id_list) AND a.payment_status!=0 AND a.lab_app_id=:id  " + 
 			"AND l.lab_id=a.lab_id AND p.patient_id=a.patient_id",nativeQuery=true)
 	GetLabBillDetails getLabBillDetailsRepository(@Param("id")int id);
 	
