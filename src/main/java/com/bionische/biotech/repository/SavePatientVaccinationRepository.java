5de81088e0c42ac6735ab77711a2266450f4d67b
Jarves <Jarves@192.168.2.10>
2019-03-27 16:40:27 +0530

    //kaushik vaccination

--- src/main/java/com/bionische/biotech/repository/SavePatientVaccinationRepository.java
+++ src/main/java/com/bionische/biotech/repository/SavePatientVaccinationRepository.java
@@ -15,7 +15,7 @@ public interface SavePatientVaccinationRepository extends JpaRepository<SavePati
 	
 	@Query(value=" SELECT CONCAT(d.f_name,\" \",d.l_name) AS string_1,v.vaccination_id,v.patient_id,v.id, v.status " + 
 			",v.doctor_id,v.int_1,v.int_2,v.created_date,v.modified_date " + 
-			" FROM vaccination_completed v, doctor_details d WHERE v.doctor_id=d.doctor_id GROUP BY v.id",nativeQuery=true)
+			" FROM vaccination_completed v, doctor_details d WHERE v.doctor_id=d.doctor_id  AND v.patient_id=:patientId GROUP BY v.id",nativeQuery=true)
 	List<SavePatientVaccination> findByPatientId(@Param("patientId")int patientId);
 	
 }
