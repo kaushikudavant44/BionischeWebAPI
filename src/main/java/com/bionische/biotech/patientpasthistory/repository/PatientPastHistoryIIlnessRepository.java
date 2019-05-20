7cca68ef16866f0b207aa09854bebcc21afe7b97
admin <bionischetech@gmail.com>
2019-05-20 17:23:23 +0530

    patient past history //Ganesh

--- src/main/java/com/bionische/biotech/patientpasthistory/repository/PatientPastHistoryIIlnessRepository.java
+++ src/main/java/com/bionische/biotech/patientpasthistory/repository/PatientPastHistoryIIlnessRepository.java
@@ -24,7 +24,7 @@ public interface PatientPastHistoryIIlnessRepository  extends JpaRepository<Pati
 	List<PatientPastHistoryIIlness> getPastHistoryIllness(@Param("patientHistoryList")List<String> patientHistoryList);
 	
 	
-	@Query(value=" SELECT i.* from past_history_illness i, patient_past_history p where p.patient_id=:patientId AND FIND_IN_SET(i.illness_id, p.past_history_illness_id)",nativeQuery=true)
+	@Query(value="SELECT i.illness_desc, i.int_2, i.string1,i.string2, i.illness_name, i.illness_id, CASE WHEN FIND_IN_SET(i.illness_id, p.past_history_illness_id) THEN 1 ELSE 0 END AS int_1 FROM past_history_illness i, patient_past_history p WHERE p.patient_id=:patientId ",nativeQuery=true)
 
 	List<PatientPastHistoryIIlness> getPastHistory(@Param("patientId")int patientId);
 }
