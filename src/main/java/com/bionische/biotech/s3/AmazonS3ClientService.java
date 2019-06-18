6123d8b51fe375c340846eb14d0d073f57f6dc0c
Admin <Admin@192.168.2.13>
2019-06-18 17:23:59 +0530

    AWS S3 //atul

---
+++ src/main/java/com/bionische/biotech/s3/AmazonS3ClientService.java
@@ -0,0 +1,12 @@
+package com.bionische.biotech.s3;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface AmazonS3ClientService {
+
+	
+
+    void uploadFileToS3Bucket(MultipartFile multipartFile, boolean enablePublicReadAccess);
+
+    void deleteFileFromS3Bucket(String fileName);
+}
