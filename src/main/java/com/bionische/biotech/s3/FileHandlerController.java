812ef2a7f7b0c48f458e51d78d0f9afcad39b89e
Admin <Admin@192.168.2.13>
2019-06-18 17:26:14 +0530

    POM //atul

--- src/main/java/com/bionische/biotech/s3/FileHandlerController.java
+++ src/main/java/com/bionische/biotech/s3/FileHandlerController.java
@@ -8,9 +8,25 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.HashMap;
 import java.util.Map;
 
+
+/*
+aws.access.key.id =AKIA2RPEIHRMV3VIZB3J
+aws.access.key.secret =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
+aws.region = ap-south-1
+aws.s3.audio.bucket =elasticbeanstalk-ap-south-1-724717550681
+*/
+
+
+
+/*
+<dependency>
+<groupId>com.amazonaws</groupId>
+<artifactId>aws-java-sdk</artifactId>
+<version>1.11.106</version>
+</dependency>*/
+
 @RestController
 @RequestMapping("/files")
-
 public class FileHandlerController {
 
 	
